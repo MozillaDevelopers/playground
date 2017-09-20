cfe10d1673985bd054b9fb2a91fa139820c57ad9
Dan Brown <slightlyoffbeat@gmail.com>
2017-09-20 02:30:17 -0700

    twitter meta update

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,7 +6,7 @@ import 'typeface-antonio';
 import faviconico from './img/favicon.ico';
 import faviconpng from './img/favicon.png';
 import facebook from './img/fb.png';
-import twitter from './img/twitter.gif';
+import twitter from './img/twitter.png';
 
 // styles
 import '../styles/style.scss';
@@ -21,17 +21,26 @@ const Layout = ({ children }) => (
     <Helmet>
       <link rel="shortcut icon" type="image/x-icon" href={faviconico} />
       <link rel="icon" type="image/png" href={faviconpng} sizes="196x196" />
-      <meta name="description" content="Learn about CSS Grid Layout and Firefox's CSS Grid Layout Tools." />
-      <meta property="og:url" content="https://mozilla.github.io/playground" />
-      <meta property="og:title" content="CSS Grid Layout + Firefox" />
-      <meta property="og:description" content="CSS Grid Layout is a game-changer for web developers. Learn more about CSS Grid and the CSS Grid Layout panel in Firefox." />
+      <meta
+        name="description"
+        content="Learn about CSS Grid Layout and Firefox's CSS Grid Layout Tools."
+      />
+      <meta property="og:url" content="https://mozilladevelopers.github.io/playground" />
+      <meta property="og:title" content="CSS Grid Layout + Firefox = ❤️" />
+      <meta
+        property="og:description"
+        content="CSS Grid Layout is a game-changer for web developers. Learn more about CSS Grid and the CSS Grid Layout panel in Firefox."
+      />
       <meta property="og:image" content={facebook} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@mozilla" />
       <meta name="twitter:creator" content="@mozilla" />
-      <meta name="twitter:title" content="CSS Grid Layout + Firefox" />
-      <meta name="twitter:description" content="CSS Grid Layout is a game-changer for web developers. Learn more about CSS Grid and the CSS Grid Layout panel in Firefox." />
-      <meta name="twitter:image" content={twitter} />
+      <meta name="twitter:title" content="CSS Grid Layout + Firefox = ❤️" />
+      <meta
+        name="twitter:description"
+        content="CSS Grid Layout is a game-changer for web developers. Learn more about CSS Grid and the CSS Grid Layout panel in Firefox."
+      />
+      <meta name="twitter:image" content="https://i.imgur.com/NLUqs7J.png" />
     </Helmet>
 
     <Header />
