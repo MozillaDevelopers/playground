c92395808f82045169ff7e501781137c67d765f8
Dan Brown <slightlyoffbeat@gmail.com>
2017-09-21 16:42:40 -0700

    Updates

---
+++ js/ga.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+(function() {
+  // If doNotTrack is not enabled, it is ok to add GTM
+  if (typeof Mozilla.dntEnabled === 'function' && !Mozilla.dntEnabled()) {
+    window.ga =
+      window.ga ||
+      function() {
+        (ga.q = ga.q || []).push(arguments);
+      };
+    ga.l = +new Date();
+    ga('create', 'UA-35433268-86', 'auto');
+
+    ga('require', 'cleanUrlTracker');
+    ga('require', 'maxScrollTracker');
+    ga('require', 'mediaQueryTracker');
+    ga('require', 'outboundLinkTracker');
+    ga('require', 'pageVisibilityTracker');
+
+    ga('send', 'pageview');
+  }
+})();
