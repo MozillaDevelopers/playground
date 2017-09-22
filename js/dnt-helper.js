3d0c6a69261b547f3f3a891224dd6507df5c1f27
Dan Brown <slightlyoffbeat@gmail.com>
2017-09-21 17:08:14 -0700

    Updates

--- js/dnt-helper.js
+++ js/dnt-helper.js
@@ -5,8 +5,9 @@
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // create namespace
+let Mozilla;
 if (typeof Mozilla === 'undefined') {
-    var Mozilla = {};
+    Mozilla = {};
 }
 
 /**
