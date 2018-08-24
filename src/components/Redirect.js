8b29ea222ba3e54aae9df4252954805d35ab8822
mottox2 <y.mottox2@gmail.com>
2018-08-25 02:38:40 +0900

    Import Link from gatsby

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
+import { Link } from 'gatsby';
 
 class Redirect extends React.Component {
   componentDidMount() {
